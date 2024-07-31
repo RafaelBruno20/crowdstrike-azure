4550bc56cd0d538086be0da6906738613e82b215
Rafael Bruno <rvasquesbruno@gmail.com>
2024-07-31 11:19:01 +0100

    goin to start a library with multiple function. think we be easier to manage

---
+++ library.ps1
@@ -0,0 +1,31 @@
+function CreateDiagnosticVm {
+    
+}
+
+function TurnOffVm {
+    param (
+        $vmList
+    )
+    
+}
+
+function CreateSnapshot {
+    param (
+        
+    )
+    
+}
+
+function CreateDisk {
+    param (
+        
+    )
+    
+}
+
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
