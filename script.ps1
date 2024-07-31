a2b57a3f1ce2f5b0dbab0b7865154ca9b4ca761a
Rafael Bruno <rvasquesbruno@gmail.com>
2024-07-31 09:30:18 +0100

    Snapshot create function

--- script.ps1
+++ script.ps1
@@ -16,26 +16,77 @@ $securePassword = ConvertTo-SecureString -String $appSecretValue
 #Turn off all VMs that are affected
 try {
    foreach ($vm in $affectedVMs) {
-        $vmStatus = Get-AzVM | Where-Object {$_.Name -match $vm} -ErrorAction SilentlyContinue
+        $vmTrue = Get-AzVM | Where-Object {$_.Name -match $vm} -ErrorAction SilentlyContinue
 
-        if ($null -eq $vmStatus) {
-            Write-Output "${$vm} does not exist. Please check the name and try again!"
+        if ($null -eq $vmTrue) {
+            Write-Output "$vm does not exist. Please check the name and try again!"
         }
         else {
-            if ($null -eq $vmStatus.StatusCode) {
-                
+            $vmStatus = Get-AzVm -Name $vmTrue.Name -Status
+
+            if ($vmStatus.Statuses[1].Code -match "running") {
+                Write-Host "${$vmTrue.Name} is running. Turning it off now..." -ForegroundColor Yellow
+
+                Stop-AzVM -Name $vmTrue.Name -ResourceGroupName $vmTrue.ResourceGroupName
+                Start-Sleep 30
+
+                Write-Host "${$vmTrue.Name} is now shutting down!" -ForegroundColor Green
             }
+            else {
+                Write-Host "${$vmTrue.Name} is already off."
+            }
+        }
+    }
+
+    Write-Host "All affected VMs have been turned off, creating a snapshot of the C drive now!" -ForegroundColor Blue
+}
+catch {
+}
+
+#Create a snapshot and a new disk of C Drive
+try {
+    foreach ($vm in $affectedVMs) {
+        $vmStatus = Get-AzVm -Name $vm -Status
+
+        #Confirm if the VM is stopped
+        if ($vmStatus.Statuses[1].Code -match "stopped") {
+            
+            Write-Host "$vm is stopped. Creating a snapshot of the C drive..." -ForegroundColor Yellow
+
+            $vmID = Get-AzVM -Name $vm
+
+            $snapName = "$vm" + "-c-snap"
+            $createOption = "Copy"
+            
+            $snapshotConfig = New-AzSnapshotConfig `
+            -SourceUri $vmId.StorageProfile.OsDisk.ManagedDisk.Id `
+            -Location $vmID.Location `
+            -CreateOption $createOption `
+            -SnapshotName $snapName `
+            -ResourceGroupName $vmID.ResourceGroupName
+
+            New-AzSnapshot -Snapshot $snapshotConfig
+
+            #Confirm if snapshot was created correctly
+
+            $snapConfirm = Get-AzSnapshot -SnapshotName $snapName
+            $snapConfirm.Name
+
+            if ($null -eq $snapConfirm) {
+                Write-Host "Snapshot $snapName was not created..." -ForegroundColor Red
+            }
+
+            Write-Host "Snapshot ${$snapConfirm.Name} was create sucessufully!" -ForegroundColor Green
+            
+        }
+        else {
+            Write-Host "Not able to create a snapshot of C Drive for $vm" -ForegroundColor Red
         }
     }
 }
 catch {
     <#Do this if a terminating exception happens#>
 }
-
-
-
-#Create a snapshot and a new disk of C Drive
-
 
 
 
